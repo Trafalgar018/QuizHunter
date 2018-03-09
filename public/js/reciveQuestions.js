519ecc2aeb6627414f4a0960c5375b1a5fc648f2
Trafalgar <eloyneolo@icloud.com>
2018-03-10 00:42:37 +0100

    ajax

--- public/js/reciveQuestions.js
+++ public/js/reciveQuestions.js
@@ -79,6 +79,11 @@ module.exports = __webpack_require__(49);
 $(document).ready(function () {
     console.log("pepe");
     $('#envQuestions').click(function () {
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
 
         $.ajax({
 
--- resources/assets/js/reciveQuestions.js
+++ resources/assets/js/reciveQuestions.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
 console.log("pepe");
     $('#envQuestions').click(function () {
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
 
         $.ajax({
 
