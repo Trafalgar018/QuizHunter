58808678cd3965a87f3a47975b87f95b0e48eb22
Trafalgar018 <eloyneolo@icloud.com>
2018-03-11 23:31:55 +0100

    Sprint final

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,7 +4,7 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-
+use \Symfony\Component\HttpKernel\Exception\HttpException;
 class Handler extends ExceptionHandler
 {
     /**
@@ -48,6 +48,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        if( ! $exception instanceof HttpException && ! config('app.debug') ){
+            $exception = new HttpException(500, $exception->getMessage(), $exception);
+        }
+        return parent::render($request, $exception);	         return parent::render($request, $exception);
     }
 }
